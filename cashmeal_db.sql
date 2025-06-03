09b097e10a3a92f63667c089e13b74b686cd0a32
Ishaan Shete <128105840+IsheteDGr8@users.noreply.github.com>
2025-06-02 17:23:06 -0700

    Update cashmeal_db.sql

--- cashmeal_db.sql
+++ cashmeal_db.sql
@@ -1,1 +1,38 @@
+CREATE TABLE "Company" (
+	"CompanyName"	VARCHAR(100),
+	"ServiceDetails"	TEXT,
+	"AvailablePlatforms"	VARCHAR(100),
+	"Logo"	VARCHAR(100),
+	"Founders"	VARCHAR(100),
+	PRIMARY KEY("CompanyName")
+);
 
+CREATE TABLE "Customer" (
+	"AccountNumber"	INT,
+	"CustomerName"	VARCHAR(100),
+	"CustomerAddress"	VARCHAR(255),
+	PRIMARY KEY("AccountNumber")
+);
+
+CREATE TABLE "EventPlanner" (
+	"ID"	INT,
+	"EventPlannerName"	VARCHAR(100),
+	"EventAddress"	VARCHAR(255),
+	"EventTime"	DATETIME,
+	PRIMARY KEY("ID")
+);
+
+CREATE TABLE "Restaurant" (
+	"RestaurantID"	INT,
+	"RestaurantName"	VARCHAR(100),
+	"EstimateCost"	DECIMAL(6, 2),
+	"EstimatePrepTime"	INT,
+	"Location"	VARCHAR(255),
+	PRIMARY KEY("RestaurantID")
+);
+
+CREATE TABLE "Reviewer" (
+	"ReviewerID"	INT,
+	"Name"	VARCHAR(100),
+	PRIMARY KEY("ReviewerID")
+);
