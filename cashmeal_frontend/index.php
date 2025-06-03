b50e6b37071ab016ad32c4ed63e2b4dc7467fc16
DjuRi <davint2004@gmail.com>
2025-06-03 00:57:31 -0700

    Updated index.php
    Just added the base code to index.php (from the example index.php file from the google drive). So now we can work on it from here on out.

--- cashmeal_frontend/index.php
+++ cashmeal_frontend/index.php
@@ -1,1 +1,15 @@
+<?php
+/**
+ * CashMeal Frontend Interface
+ * Created By: Davin Til, (y'alls names separated by quotes)
+ * CSS 475 - Database System
+ * 
+ */
+>
+<html>
+<head>
+  <title> CashMeal </title>
+  <link rel="stylesheet" href="base.css">
+</head>
+</html>
 
