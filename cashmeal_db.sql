974d12a39bd13a6deb7dd1eb35754dbba0ec7d66
Ishaan Shete <128105840+IsheteDGr8@users.noreply.github.com>
2025-06-02 23:17:32 -0700

    Update cashmeal_db.sql

--- cashmeal_db.sql
+++ cashmeal_db.sql
@@ -70,3 +70,57 @@ CREATE TABLE MenuItem (
     EstimatePrepTime INTEGER,
     FOREIGN KEY (RestaurantID) REFERENCES Restaurant(RestaurantID)
 );
+
+
+
+-- Adding Sample Data
+
+INSERT INTO Restaurant VALUES (10000001, 'Taco Bell', 8.50, 10, 'San Diego, CA');
+INSERT INTO Restaurant VALUES (10000002, 'Chipotle Mexican Grill', 11.00, 12, 'Denver, CO');
+INSERT INTO Restaurant VALUES (10000003, 'McDonald''s', 9.00, 8, 'Chicago, IL');
+INSERT INTO Restaurant VALUES (10000004, 'Subway', 7.50, 6, 'Milford, CT');
+INSERT INTO Restaurant VALUES (10000005, 'Chick-fil-A', 9.50, 9, 'Atlanta, GA');
+INSERT INTO Restaurant VALUES (10000006, 'Panda Express', 10.00, 10, 'Rosemead, CA');
+INSERT INTO Restaurant VALUES (10000007, 'Wendy''s', 8.75, 7, 'Dublin, OH');
+INSERT INTO Restaurant VALUES (10000008, 'Burger King', 8.25, 7, 'Miami, FL');
+INSERT INTO Restaurant VALUES (10000009, 'KFC', 9.2, 8, 'Louisville, KY');
+INSERT INTO Restaurant VALUES (10000010, 'Domino''s Pizza', 11.50, 15, 'Ann Arbor, MI');
+
+
+-- Taco Bell Menu
+INSERT INTO MenuItem (RestaurantID, ItemName, ItemPrice, EstimatePrepTime) VALUES
+(10000001, 'Crunchy Taco', 1.69, 5),
+(10000001, 'Chicken Quesadilla', 4.99, 7),
+(10000001, 'Beef Burrito', 2.49, 6),
+(10000001, 'Nachos BellGrande', 4.89, 8),
+(10000001, 'Cheesy Gordita Crunch', 4.59, 7),
+(10000001, 'Chalupa Supreme', 3.99, 7),
+(10000001, 'Bean Burrito', 1.49, 5),
+(10000001, 'Cinnamon Twists', 1.00, 2),
+(10000001, 'Baja Blast (Medium)', 2.29, 1),
+(10000001, 'Mexican Pizza', 5.49, 9);
+
+-- Chipotle Menu
+INSERT INTO MenuItem (RestaurantID, ItemName, ItemPrice, EstimatePrepTime) VALUES
+(10000002, 'Burrito Bowl', 9.25, 10),
+(10000002, 'Chicken Burrito', 9.50, 11),
+(10000002, 'Chips & Guac', 4.25, 3),
+(10000002, 'Veggie Tacos', 8.75, 10),
+(10000002, 'Fountain Drink', 2.25, 1);
+
+-- McDonald's Menu
+INSERT INTO MenuItem (RestaurantID, ItemName, ItemPrice, EstimatePrepTime) VALUES
+(10000003, 'Big Mac', 5.69, 6),
+(10000003, 'Medium Fries', 2.89, 3),
+(10000003, '10 pc McNuggets', 5.49, 4),
+(10000003, 'Filet-O-Fish', 4.89, 5),
+(10000003, 'McFlurry (Oreo)', 3.89, 2);
+
+-- Chick-fil-A Menu
+INSERT INTO MenuItem (RestaurantID, ItemName, ItemPrice, EstimatePrepTime) VALUES
+(10000005, 'Chicken Sandwich', 4.69, 5),
+(10000005, 'Waffle Fries', 2.35, 4),
+(10000005, 'Spicy Chicken Deluxe', 5.75, 6),
+(10000005, 'Chick-n-Strips (3 pc)', 4.85, 6),
+(10000005, 'Iced Tea', 1.89, 1);
+
